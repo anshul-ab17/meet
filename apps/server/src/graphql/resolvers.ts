c509a0090a840084d274eed0066892c90d8364d7
Anshul Bharat <anshul.ab17x@gmail.com>
2026-07-17 15:13:18 +0530

    feat(server/graphql): extend schema and implement resolvers for users, chats, and friends

--- apps/server/src/graphql/resolvers.ts
+++ apps/server/src/graphql/resolvers.ts
@@ -1,16 +1,60 @@
-import { UserService, ChatService, MessageService } from "@repo/db";
+import { UserService, ChatService, MessageService, FriendService } from "@repo/db";
 import { v4 as uuidv4 } from "uuid";
+import { GraphQLError } from "graphql";
+import { roomManager } from "../ws/roomManager.js";
 
 const userService = new UserService();
 const chatService = new ChatService();
 const messageService = new MessageService();
+const friendService = new FriendService();
 
-export const resolvers = {
+interface Context {
+  user?: {
+    id: string;
+    name: string;
+  };
+}
+
+function checkAuth(context: Context) {
+  if (!context.user) {
+    throw new GraphQLError("Unauthorized", {
+      extensions: { code: "UNAUTHENTICATED" },
+    });
+  }
+  return context.user;
+}
+
+export const resolvers: any = {
   Query: {
     user: (_: unknown, { id }: { id: string }) => userService.getUser(id),
-    rooms: () => chatService.getRooms(),
-    messages: (_: unknown, { chatId }: { chatId: string }) =>
-      messageService.getMessages(chatId),
+    userByName: async (_: unknown, { name }: { name: string }, context: Context) => {
+      checkAuth(context);
+      return userService.getUserByName(name);
+    },
+    globalRoom: async (_: unknown, __: unknown, context: Context) => {
+      checkAuth(context);
+      return chatService.getOrCreateGlobal();
+    },
+    channels: async (_: unknown, __: unknown, context: Context) => {
+      checkAuth(context);
+      return chatService.getChannels();
+    },
+    messages: async (_: unknown, { chatId }: { chatId: string }, context: Context) => {
+      checkAuth(context);
+      return messageService.getMessages(chatId);
+    },
+    friends: async (_: unknown, __: unknown, context: Context) => {
+      const user = checkAuth(context);
+      return friendService.getFriends(user.id);
+    },
+    pendingRequests: async (_: unknown, __: unknown, context: Context) => {
+      const user = checkAuth(context);
+      return friendService.getPendingRequests(user.id);
+    },
+    sentRequests: async (_: unknown, __: unknown, context: Context) => {
+      const user = checkAuth(context);
+      return friendService.getSentRequests(user.id);
+    },
   },
 
   Mutation: {
@@ -18,14 +62,55 @@ export const resolvers = {
       const id = uuidv4();
       return userService.createUser(id, name);
     },
-    createRoom: (_: unknown, { name }: { name: string }) =>
-      chatService.createChannel(name),
+    createChannel: async (_: unknown, { name }: { name: string }, context: Context) => {
+      checkAuth(context);
+      const room = await chatService.createChannel(name);
+      roomManager.broadcastAll({ type: "channel-created", channel: room });
+      return room;
+    },
+    getOrCreateDM: async (_: unknown, { targetUserId }: { targetUserId: string }, context: Context) => {
+      const user = checkAuth(context);
+      return chatService.getOrCreateDM(user.id, targetUserId);
+    },
     joinRoom: async (
       _: unknown,
-      { userId, chatId }: { userId: string; chatId: string }
+      { userId, chatId }: { userId: string; chatId: string },
+      context: Context
     ) => {
+      checkAuth(context);
       await chatService.joinRoom(userId, chatId);
       return true;
+    },
+    sendFriendRequest: async (_: unknown, { targetUserId }: { targetUserId: string }, context: Context) => {
+      const user = checkAuth(context);
+      await friendService.sendRequest(user.id, targetUserId);
+      roomManager.sendToUser(targetUserId, { type: "friend-update" });
+      return true;
+    },
+    acceptFriendRequest: async (_: unknown, { requesterId }: { requesterId: string }, context: Context) => {
+      const user = checkAuth(context);
+      await friendService.acceptRequest(requesterId, user.id);
+      roomManager.sendToUser(requesterId, { type: "friend-update" });
+      return true;
+    },
+    rejectFriendRequest: async (_: unknown, { requesterId }: { requesterId: string }, context: Context) => {
+      const user = checkAuth(context);
+      await friendService.rejectRequest(requesterId, user.id);
+      return true;
+    },
+    cancelFriendRequest: async (_: unknown, { targetUserId }: { targetUserId: string }, context: Context) => {
+      const user = checkAuth(context);
+      await friendService.cancelRequest(user.id, targetUserId);
+      return true;
+    },
+    removeFriend: async (_: unknown, { friendId }: { friendId: string }, context: Context) => {
+      const user = checkAuth(context);
+      await friendService.removeFriend(user.id, friendId);
+      return true;
+    },
+    updateProfile: async (_: unknown, { bio }: { bio: string }, context: Context) => {
+      const user = checkAuth(context);
+      return userService.updateProfile(user.id, bio);
     },
   },
 };
--- apps/server/src/graphql/schema.ts
+++ apps/server/src/graphql/schema.ts
@@ -2,11 +2,13 @@ export const typeDefs = `#graphql
   type User {
     id: ID!
     name: String!
+    bio: String
   }
 
   type Room {
     chatId: ID!
     name: String!
+    type: String!
   }
 
   type Message {
@@ -19,13 +21,25 @@ export const typeDefs = `#graphql
 
   type Query {
     user(id: ID!): User
-    rooms: [Room!]!
+    userByName(name: String!): User
+    globalRoom: Room!
+    channels: [Room!]!
     messages(chatId: ID!): [Message!]!
+    friends: [User!]!
+    pendingRequests: [User!]!
+    sentRequests: [User!]!
   }
 
   type Mutation {
     createUser(name: String!): User!
-    createRoom(name: String!): Room!
+    createChannel(name: String!): Room!
+    getOrCreateDM(targetUserId: ID!): Room!
     joinRoom(userId: ID!, chatId: ID!): Boolean!
+    sendFriendRequest(targetUserId: ID!): Boolean!
+    acceptFriendRequest(requesterId: ID!): Boolean!
+    rejectFriendRequest(requesterId: ID!): Boolean!
+    cancelFriendRequest(targetUserId: ID!): Boolean!
+    removeFriend(friendId: ID!): Boolean!
+    updateProfile(bio: String!): User!
   }
 `;
